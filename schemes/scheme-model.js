352b732cfa3579b74b4828e304a41793f2f74869
Sean Kramer <seandominickramer@gmail.com>
2020-10-20 18:29:47 -0400

    Add findSteps function

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -6,11 +6,31 @@ return db('schemes')
 
 function findById(id) {
   return db("schemes")
-  .where({ id })
+  .where('id', id)
   .first()
+}
+
+function findSteps(id) {
+return db('steps as st')
+.join('schemes as sc', 'st.scheme_id', 'sc.id')
+.where('scheme_id', id)
+.select('st.id', 'sc.scheme_name', 'st.step_number', 'st.instructions')
+}
+
+function add(scheme) {
+
+}
+
+function update(changes, id) {
+
+}
+
+function remove(id) {
+
 }
 
 module.exports = {
   find,
   findById,
+  findSteps,
 }
